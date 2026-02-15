2a620674e3d2f11e872d9078327cd4488cddd0d7
Bhavya7448 <bhavyasharma011076@gmail.com>
2026-02-15 23:47:35 +0530

    Add files via upload

---
+++ cppBubbleSort2dArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[10][10], r, c;
+
+    cout<<"Enter rows and columns: ";
+    cin>>r>>c;
+
+    cout<<"Enter elements:\n";
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            cin>>a[i][j];
+
+    // Bubble sort each row
+    for(int i=0;i<r;i++){
+        for(int pass=0;pass<c-1;pass++){
+            for(int j=0;j<c-pass-1;j++){
+                if(a[i][j] > a[i][j+1]){
+                    int temp = a[i][j];
+                    a[i][j] = a[i][j+1];
+                    a[i][j+1] = temp;
+                }
+            }
+        }
+    }
+
+    cout<<"Sorted Matrix (Row-wise):\n";
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+            cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+}
