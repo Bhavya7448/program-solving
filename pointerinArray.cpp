3942ed3fa2b6caf9f018a0b7abdb61804889a142
Bhavya7448 <bhavyasharma011076@gmail.com>
2026-02-16 22:11:57 +0530

    Add files via upload

---
+++ pointerinArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3] = {10, 20, 30};
+    int *p = arr;
+
+    for(int i = 0; i < 3; i++)
+    {
+        cout << *(p + i) << " ";
+    }
+
+    return 0;
+}
