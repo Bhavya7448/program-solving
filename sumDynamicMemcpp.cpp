df2b2e74735a3862c2a7f104e22d4c4abde3da49
Bhavya7448 <bhavyasharma011076@gmail.com>
2026-02-17 20:50:29 +0530

    Add files via upload

---
+++ sumDynamicMemcpp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    int *arr = new int[n];
+
+    cout << "Enter elements: ";
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < n; i++)
+        sum += arr[i];
+
+    cout << "Sum = " << sum;
+
+    delete[] arr;
+
+    return 0;
+}
