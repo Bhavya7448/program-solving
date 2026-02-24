bdbd453510f7e67abbe6ebe80a994dc08c66ecf7
Bhavya7448 <bhavyasharma011076@gmail.com>
2026-02-24 22:14:43 +0530

    Add files via upload

---
+++ concatanateString.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int i=0, j=0;
+
+    gets(str1);
+    gets(str2);
+
+    while(str1[i] != '\0')
+        i++;
+
+    while(str2[j] != '\0') {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+    str1[i] = '\0';
+
+    printf("Concatenated string: %s", str1);
+    return 0;
+}
